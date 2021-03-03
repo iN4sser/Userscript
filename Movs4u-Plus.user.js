8d2872b195be2ce75cd4a16eff3968196980f627
iN4sser <iN4sser@users.noreply.github.com>
2021-03-03 14:04:53 +0300

    Update Movs4u-Plus.user.js

--- Movs4u-Plus.user.js
+++ Movs4u-Plus.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name        Movs4u+
 // @namespace   https://www.in4sser.com
-// @match       *://www.movs4u.*/download_link/*
+// @include       *://*.movs4u.*/*
 // @grant       none
 // @version     1.0
 // @author      iN4sser
@@ -11,7 +11,11 @@
 // @downloadURL https://raw.githubusercontent.com/iN4sser/Userscript/raw/main/Movs4u-Plus.user.js
 // @description This script will include some mods and fixes for movs4u website
 // ==/UserScript==
-var imgs = document.getElementsByTagName("img");
+
+// Skip The Download Redirection
+if (window.location.href.indexOf('movs4u' && 'download_link') > 0) {
+    var imgs = document.getElementsByTagName("img");
 for(var i=0; i<imgs.length; i++) {
     if(imgs[i].src == "https://www.movs4u.cc/d/d.png") imgs[i].click();
+}
 }
