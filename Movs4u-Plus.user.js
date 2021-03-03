91ff11f641728cf43dfc4bbbed5fc2b0efbb47ef
iN4sser <iN4sser@users.noreply.github.com>
2021-03-03 17:35:43 +0300

    Update Movs4u-Plus.user.js

--- Movs4u-Plus.user.js
+++ Movs4u-Plus.user.js
@@ -19,3 +19,44 @@ for(var i=0; i<imgs.length; i++) {
     if(imgs[i].src == "https://www.movs4u.cc/d/d.png") imgs[i].click();
 }
 }
+
+// Styling
+GM_addStyle ( `
+div#dt_contenedor, body{
+  direction:rtl !important;
+}
+.module .sidebar{
+  float: left;
+  right: unset;
+  left: 0;
+}
+.module .content{
+  float: right;
+  margin-right: unset;
+  margin-left: 340px;
+}
+.dt_mainmeta nav.genres ul li i, .dt_mainmeta nav h2:before, .module .content header span, .headitems{
+  float: left;
+}
+.module .content header h1, .module .content header h2, header.main .hbox .logo, .head-main-nav ul.main-header li a, .head-main-nav ul.main-header li, .head-main-nav ul.main-header, .head-main-nav, .dtsingle .content, ul.smenu, .single_tabs, #seasons .se-c .se-q span.se-t, #seasons .se-c .se-q, #single .content .sbox, #serie_contenido, #seasons, #seasons .se-c, #seasons .se-c .se-a ul.episodios li .episodiotitle, #seasons .se-c .se-a ul.episodios li .imagen, #seasons .se-c .se-a ul.episodios li .numerando, ul.smenu li, .sbox .custom_fields, .sbox .custom_fields b.variante, .owl-carousel .owl-item{
+  float: right;
+}
+header.main .hbox .search form input[type=text]{
+  text-align: center;
+}
+.dtsingle .sidebar{
+  position: relative;
+}
+#seasons .se-c .se-q{
+  padding-right: 105px;
+  padding-left: unset;
+}
+#seasons .se-c .se-a ul.episodios li .episodiotitle{
+  padding-right: 190px;
+  padding-left: unset;
+}
+#seasons .se-c .se-q span.se-t, .owl-wrapper{
+  right: 0;
+  left: unset;
+}
+` );
