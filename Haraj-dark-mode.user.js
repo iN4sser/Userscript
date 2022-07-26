6983cc043eed71f8959614606cc6807505c85003
iN4sser <iN4sser@users.noreply.github.com>
2022-07-26 11:52:47 +0300

    Update Haraj-dark-mode.user.js

--- Haraj-dark-mode.user.js
+++ Haraj-dark-mode.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://www.in4sser.com
 // @match       *://haraj.com.sa/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      iN4sser
 // @grant       GM_addStyle
 // @icon        https://v8-cdn.haraj.com.sa/assets/images/icon_96x96.5e07e9b4868f8e0113751d416f54a860.png
@@ -16,13 +16,13 @@ GM_addStyle ( `
 body, .header, .fFKWfs, #bodyTEXT, .ddm_wrapper{
   background: #15202b !important;
 }
-.kiyHpe, .iGsYXQ, .post.even, .headLinks, input.form-control, .postWrapper .postMain .details, .postWrapper .postMain .add_comment_wrapper textarea, .postWrapper .postMain .bottom_meta_wrapper .post_id_wrapper, ul.tabs_list.supTabs.list-container, .follow-switch-wrap, .singleContent, .postWrapper .postMain .comment_list_wrapper .comment_wrapper{
+.kiyHpe, .iGsYXQ, .post.even, .headLinks, input.form-control, .postWrapper .postMain .details, .postWrapper .postMain .add_comment_wrapper textarea, .postWrapper .postMain .bottom_meta_wrapper .post_id_wrapper, ul.tabs_list.supTabs.list-container, .follow-switch-wrap, .singleContent, .postWrapper .postMain .comment_list_wrapper .comment_wrapper, .profile_content #profile_container .profile_header .cover{
   background-color: rgb(30, 39, 50) !important;
 }
-.post.odd, .postWrapper .postMain .details .post_header .post_header_wrapper{
+.post.odd, .postWrapper .postMain .details .post_header .post_header_wrapper, .profile_content #profile_container .profile_header .content{
   background-color: #15202b !important;
 }
-footer, .Box_unique121, .postWrapper .postMain .actions_wrapper, .postWrapper .postMain .tags_wrapper, .random-help-message, .addPost{
+footer, .Box_unique121, .postWrapper .postMain .actions_wrapper, .postWrapper .postMain .tags_wrapper, .random-help-message, .addPost, .chat .messenger .message-list{
   background: rgb(30, 39, 50) !important;
 }
 .btn-info, .btn-primary, button.btn-info, button.btn-primary, input.btn-info, input.btn-primary, .Badge_123, .btn-link, a.next_post_btn, .postWrapper .postMain .tags_wrapper a, .postWrapper .postMain .tags_wrapper a:visited, .postWrapper .postMain .actions_wrapper .action_btn:hover, .btn-success, button.btn-success, input.btn-success, .tag-filters__item.btn, button.reply_btn, .icon_wrapper_com, span.ddm_option:hover{
@@ -64,5 +64,11 @@ footer, .Box_unique121, .postWrapper .postMain .actions_wrapper, .postWrapper .p
 }
 .postWrapper .postMain .comment_list_wrapper .comment_wrapper .comment_body{
   color: white;
+}
+.chat .topics ul li.topic.selected {
+    background-color: rgb(0 50 84) !important;
+}
+.userWrapper{
+  background-color: unset;
 }
 ` );
