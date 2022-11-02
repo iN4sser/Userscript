96b50f692c61813ee1effec6dd9b62bd71b5edd5
iN4sser <iN4sser@users.noreply.github.com>
2022-11-02 13:02:21 +0300

    Update Whatsapp-POS.user.js

--- Whatsapp-POS.user.js
+++ Whatsapp-POS.user.js
@@ -4,7 +4,7 @@
 // @namespace   Violentmonkey Scripts
 // @include     *://api.whatsapp.com/*
 // @include     *://web.whatsapp.com/*
-// @version     1.7
+// @version     1.8
 // @author      iN4sser
 // @namespace   iN4sser
 // @grant       GM_addStyle
@@ -27,7 +27,7 @@ window.onload=function(){
   setInterval(autoClick,100);
 }
 function autoClick(){   
-  document.querySelector("#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div._3HQNh._1Ae7k").click();
+  document.querySelector("#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div._3HQNh._1Ae7k > button").click();
   setTimeout(window.close,7000);
 }
 
