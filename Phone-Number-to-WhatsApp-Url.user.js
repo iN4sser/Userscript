4b361310658516d9e6b54dac4b2d20d11382b863
iN4sser <iN4sser@users.noreply.github.com>
2022-12-08 15:53:50 +0300

    Update Phone-Number-to-WhatsApp-Url.user.js

--- Phone-Number-to-WhatsApp-Url.user.js
+++ Phone-Number-to-WhatsApp-Url.user.js
@@ -2,7 +2,7 @@
 // @name        Phone Number to WhatsApp Url
 // @namespace   https://www.in4sser.com
 // @match       *://*/*
-// @version     1.0
+// @version     1.1
 // @author      iN4sser
 // @grant       GM_addStyle
 // @run-at      document-end
@@ -12,6 +12,12 @@
 // @description هذا السكربت سيقوم بتحويل ارقام الجوالات في اي موقع إلى روابط واتس اب
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // ==/UserScript==
-jQuery('a').each(function() {
-    this.href = this.href.replace("tel:", "https://web.whatsapp.com/send/?phone=");
+document.addEventListener('DOMSubtreeModified', function () {
+    document.querySelectorAll('a[href^="tel:"]').forEach(function (a) {
+        a.href = a.href.replace(/^tel:/, 'https://wa.me/');
+    });
 });
+// check if anything on the page has changed every 100ms and replace the tel: links with whatsapp links
+setInterval(function () {
+    document.dispatchEvent(new Event('DOMSubtreeModified')); // trigger the event
+}, 100); // check every 100ms
