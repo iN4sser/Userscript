b2f74f34f5e2a96f7e817c0930ff330d7bf31324
iN4sser <iN4sser@users.noreply.github.com>
2023-05-03 11:09:55 +0300

    Update Haraj-auto-refresh.user.js

--- Haraj-auto-refresh.user.js
+++ Haraj-auto-refresh.user.js
@@ -4,11 +4,9 @@
 // @namespace   https://www.in4sser.com
 // @match       *://haraj.com.sa/*
 // @grant       none
-// @version     1.5.1
+// @version     1.5.2
 // @author      iN4sser
 // @grant       GM_addStyle
-// @updateURL   https://github.com/iN4sser/Userscript/raw/main/Haraj-auto-refresh.user.js
-// @downloadURL https://github.com/iN4sser/Userscript/raw/main/Haraj-auto-refresh.user.js
 // @description هذا السكربت سيقوم بتحديث اعلاناتك مباشرة بمجرد الدخول عليها
 // @description:en This script will auto-update any post you open in your browser
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
@@ -17,7 +15,7 @@
 setTimeout(function() {
  Array.prototype.forEach.call(document.getElementsByTagName('button'), function(elem) {
 	if (elem.innerHTML.indexOf('تحديث') > -1) {
-		document.querySelector("#__next > div > div.px-3.xl\\:px-0.mx-auto.grid.max-w-7xl.grid-cols-1.gap-2.lg\\:grid-cols-4 > div.col-span-full.md\\:col-span-3 > div:nth-child(4) > div > div > button:nth-child(2)").click()
+		document.querySelector("#__next > div > div.grid.max-w-7xl.grid-cols-1.gap-2.px-2.md\\:mx-auto.lg\\:grid-cols-4 > div.col-span-full.md\\:col-span-3 > div:nth-child(4) > div > div > button:nth-child(2)").click()
 	}
 });
-}, 2000);
+}, 2500);
